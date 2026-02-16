68bf2c6327772f414b9ddfd36866cd54f5bc6b39
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2026-02-16 12:46:17 +0100

    Réunion des templates communs

--- public/product.php
+++ public/product.php
@@ -1,29 +1,9 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <title>Gibson Les Paul Standard - Rock Station</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Gibson Les Paul Standard 60s - Guitare électrique rock avec table érable et micros Burstbucker.">
-    <link rel="stylesheet" href="resources/css/main.css">
-    <link rel="stylesheet" href="resources/css/product.css">
-</head>
-<body>
-
-<header class="site-header">
-    <div class="container header-flex">
-        <p class="logo"><a href="products.html">Rock Station</a></p>
-        <!-- <nav class="main-nav">
-            <a href="#">Guitares</a>
-            <a href="#">Amplis</a>     
-        </nav> -->
-        <nav class="user-nav">
-            <a href="#" class="basket-link" title="Voir le panier">🛒</a>
-        </nav>
-    </div>
-</header>
-
-<main class="container">
+<?php
+$title = 'Gibson Les Paul Standard - Rock Station';
+$description = 'Gibson Les Paul Standard 60s - Guitare électrique rock avec table érable et micros Burstbucker.';
+$specificCssFilePath = 'resources/css/product.css';
+include __DIR__ . ' /../templates/header.php';
+?>
 
     <article class="product">
 
@@ -77,13 +57,6 @@
 
     </article>
 
-</main>
-
-<footer class="site-footer">
-    <div class="container">
-        <p>&copy; 2026 RockStation - Tous droits réservés</p>
-    </div>
-</footer>
-
-</body>
-</html>
+<?php
+include __DIR__ . ' /../templates/footer.php';
+?>
---
+++ templates/footer.php
@@ -0,0 +1,10 @@
+</main>
+
+<footer class="site-footer">
+    <div class="container">
+        <p>&copy; 2026 RockStation - Tous droits réservés</p>
+    </div>
+</footer>
+
+</body>
+</html>
